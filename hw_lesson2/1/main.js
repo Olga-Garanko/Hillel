8780c9f9c4c3ff1e5ea9aef0f7a4be6252a10b36
Olga-Garanko <olga.garanko@gmail.com>
2019-02-14 08:38:56 +0200

    fibonachi

---
+++ hw_lesson2/1/main.js
@@ -0,0 +1,16 @@
+do {
+	n = +prompt('Введите число n!', 0)
+} while ((isNaN(n)) || (n<=0));
+
+if (n==1) { sum = 1}
+else if (n==2) { sum = 2}
+else {
+	for (i=3, prev1=1, prev2=1, sum=2; i <= n; i++){
+		num = prev1 + prev2;
+		prev1 = prev2;
+		prev2 = num;
+		sum = sum + num;
+	}
+}
+
+console.log(sum);
