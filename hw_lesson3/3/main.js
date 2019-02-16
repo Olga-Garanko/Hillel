66b65b2bbb9cd85acec6f5af6dff49b46eb61095
Olga-Garanko <olga.garanko@gmail.com>
2019-02-16 17:32:48 +0200

    hw3_3

--- hw_lesson3/3/main.js
+++ hw_lesson3/3/main.js
@@ -1,13 +1,12 @@
-// Домашнее задание 7
-// #2
-// 2) Элементы массива между min -- max записать в массив B. Массив взять из 1го задания
+// Домашнее задание 8
+// #3
+// В массиве A поменять местами min и max. Массив взять из 2го задания
 
 do {
 	n = +prompt('Введите размерность массива:', 0)
 } while ((isNaN(n)) || (n<=0));
 
 A = [];
-B = [];
 
 for (i=0; i < n; i++){
 	A[i] = Math.floor(Math.random()*501 - 1);
@@ -24,14 +23,19 @@ for (k=0; k < n; k++){
 console.log('max', max, 'maxIndex', maxIndex);
 console.log('min', min, 'minIndex', minIndex);
 
-if (minIndex > maxIndex) {startIndex = maxIndex; finishIndex = minIndex;}
-else { startIndex = minIndex; finishIndex = maxIndex;}
+tempValue = A[maxIndex];
+A[maxIndex] = A[minIndex];
+A[minIndex] = tempValue;
+console.log('новый массив A', A);
 
-for (var j = startIndex; j <= finishIndex; j++) {
-	B[B.length] = A[j];
-}
+/*a = 8;
+b = 3;
+a = a + b; 
+b = a - b; 
+a = a - b;
+console.log(a);
+console.log(b);*/
 
-console.log('массив B', B);
 
 
 
