f345c1849eae02886c986e2a8a4c77040ca091d6
Olga-Garanko <olga.garanko@gmail.com>
2019-02-25 00:49:29 +0200

    hw5_2

---
+++ hw_lesson5/2/main.js
@@ -0,0 +1,43 @@
+// Домашнее задание 12
+// #2
+// Написать функцию `doMath(x, znak, y)`, которая получает 3 аргумента: числа `x` и `y`, строку `znak`.
+// В переменной знак может быть: `+, -, *, /, %, ^`.
+// Вывести результат математического действия, указанного в переменной `znak`.
+do {
+	x = +prompt('Введите первый операнд:')
+} while (isNaN(x));
+do {
+	znak = prompt('Введите оператор:');
+} while (!((znak == '+') || (znak == '-') || (znak == '*') || (znak == '/') || (znak == '%') || (znak == '^')));
+do {
+	y = +prompt('Введите второй операнд:')
+} while (isNaN(y));
+
+function doMath(op1, operator, op2){
+	switch(operator) {
+	  case '+':
+	    result = op1 + op2;
+	    break;
+	  case '-':
+		result = op1 - op2;
+		break;
+	  case '*':
+	    result = op1 * op2;
+	    break;
+	  case '/':
+	  	if (op2 == 0) { console.log("Деление на 0 невозможно"); result = underfined;}
+	  	else { result = op1 / op2; }
+	  	break;
+	  case '%':
+	    result = op1 % op2;
+	    break;
+	  case '^':
+	    result = op1 ^ op2;
+	    break;
+	  default:
+	  	result = 'error';
+	}
+	return result;
+}
+
+console.log(doMath(x, znak, y));
