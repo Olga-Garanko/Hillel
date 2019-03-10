e5a30577a6aedd88876bd2e35d04d0a97e2cc12c
Olga-Garanko <olga.garanko@gmail.com>
2019-03-10 12:11:08 +0200

    hw8_2

--- hw_lesson8/2/main.js
+++ hw_lesson8/2/main.js
@@ -1,3 +1,12 @@
 /*Домашнее задание 19
 #2
 Создать функцию, которая принимает объект, меняет местами ключ и значение и возвращает новый объект. {name: 'mykola', age: 56} -> {mykola: 'name', 56: 'age'}*/
+function revertObject(obj) {
+	let objReversed = {}
+	console.log('Исходный объект', obj)
+	Object.keys(obj).forEach((key) => {
+		objReversed[obj[key]] = key
+	})
+	return objReversed
+}
+console.log('Результат', revertObject({name: 'mykola', age: 56, valid: false}))
