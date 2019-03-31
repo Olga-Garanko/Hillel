c37b0b3dad220dbef5c6f3b28d9dcb9a85929925
Olga-Garanko <olga.garanko@gmail.com>
2019-03-31 16:33:46 +0300

    hw13_1_new

--- hw_lesson13/1/main.js
+++ hw_lesson13/1/main.js
@@ -8,8 +8,63 @@
 (75 - 100)  -> 8%
 Красный блок отображает количество комиссии. Например Значение выбора 100, комиссия будет 8%. Результирующая сумма: 108. Высота красного блока - 8px*/
 
-
 let onLoad = () => {
+	let numberInput = document.querySelector('.number-input')
+	let rangeInput = document.querySelector('.range-input')
+	let diagramValue = document.querySelector('.diagram__value')
+	let diagramPercent = document.querySelector('.diagram__percent')
+	let ranges = [
+		{
+			min: 0,
+			max: 20,
+			percent: 2
+		},
+		{
+			min: 20,
+			max: 50,
+			percent: 4
+		},
+		{
+			min: 50,
+			max: 75,
+			percent: 6
+		},
+		{
+			min: 75,
+			max: 100,
+			percent: 8
+		}
+	]
+
+	let calculate = (input) => {
+		diagramValue.style.height = input + 'px'
+		ranges.forEach(range => {
+			if ((input > range.min) && (input <= range.max)) {
+				diagramPercent.style.height = input*range.percent/100 + 'px'
+			}
+		}) 
+	}
+
+	let init = () => {
+		let input = numberInput.value ? numberInput.value : 0
+		rangeInput.value = input
+		calculate(input)
+	}
+
+	let change = (e) => {
+		let input = numberInput.value = rangeInput.value = e.target.value
+		calculate(input)
+	}
+
+	numberInput.addEventListener('input', change)
+	rangeInput.addEventListener('input', change)
+	
+	init()
+}
+window.addEventListener('load', onLoad)
+
+// old method
+/*let onLoad = () => {
 	let numberInput = document.querySelector('.number-input')
 	let rangeInput = document.querySelector('.range-input')
 	let diagramValue = document.querySelector('.diagram__value')
@@ -19,13 +74,17 @@ let onLoad = () => {
 	let calculate = (input) => {
 		diagramValue.style.height = input + 'px'
 		if ((input > 75) && (input <= 100)) percent = 8
-		if ((input > 50) && (input <= 75)) percent = 8
-		if ((input > 20) && (input <= 50)) percent = 6
-		if ((input > 0) && (input <= 20)) percent = 4
+		if ((input > 50) && (input <= 75)) percent = 6
+		if ((input > 20) && (input <= 50)) percent = 4
+		if ((input > 0) && (input <= 20)) percent = 2
 		diagramPercent.style.height = input*percent/100 + 'px'
 	}
 
-	calculate(80)
+	let init = () => {
+		let input = numberInput.value ? numberInput.value : 0
+		rangeInput.value = input
+		calculate(input)
+	}
 
 	let change = (e) => {
 		let input = numberInput.value = rangeInput.value = e.target.value
@@ -34,5 +93,7 @@ let onLoad = () => {
 
 	numberInput.addEventListener('input', change)
 	rangeInput.addEventListener('input', change)
+
+	init()
 }
-window.addEventListener('load', onLoad)
+window.addEventListener('load', onLoad)*/
