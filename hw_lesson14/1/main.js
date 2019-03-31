98aa066b0cd30094e686ef1c3f6323929ffd2382
Olga-Garanko <olga.garanko@gmail.com>
2019-04-01 00:18:54 +0300

    hw14_1

--- hw_lesson14/1/main.js
+++ hw_lesson14/1/main.js
@@ -6,7 +6,34 @@
 	
 	Персонаж - это квадрат, имеющий размеры 100 на 100 пикселей.
 	Прыжок - это анимация движения объекта вверх на `h` пикселей и возврат в изначальное положение (до прыжка)*/
+let platformer = document.querySelector('.platformer')
+const step = 40
+const height = 50
+let styles = window.getComputedStyle(platformer)
+platformer.style.top = parseInt(styles.top) + 'px';
+platformer.style.left = parseInt(styles.left) + 'px';
 
+function move(e) {
+	switch(e.keyCode) {
+		case 32: 
+			platformer.style.top = parseInt(platformer.style.top) - height + 'px'
+			setTimeout(() => platformer.style.top = parseInt(platformer.style.top) + height + 'px', 500);
+			break;
+		case 37:	// клавиша влево
+			platformer.style.left = parseInt(platformer.style.left) - step + 'px'
+			break;
+		case 38:	 // клавиша вверх
+			platformer.style.top = parseInt(platformer.style.top) - step + 'px';
+			break;
+		case 39:	 // клавиша вправо
+			platformer.style.left = parseInt(platformer.style.left) + step + 'px'
+			break;
+		case 40:	 // клавиша вниз
+			platformer.style.top = parseInt(platformer.style.top) + step + 'px';
+			break;
+	}
+}
+document.addEventListener('keydown', move)
 /*Домашнее задание 26
 #2
 При нажатии на CTRL персонаж должен "присесть" (уменьшиться в размерах по высоте на 40%, по ширине - увеличится на 15%)*/
